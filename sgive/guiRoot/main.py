2e254ac893ad3efc82c5b9e95978a391e6ce2660
ryuseless <janfilipzacek@seznam.cz>
2023-09-20 13:40:14 +0200

    budiž třídy!

--- sgive/guiRoot/main.py
+++ sgive/guiRoot/main.py
@@ -1,40 +1,89 @@
 import tkinter
-from tkinter import font as tkFont
+from tkinter import *
+import tkinter.font as font
 
-masterWindow = tkinter.Tk()
-screen_width = masterWindow.winfo_screenwidth()
-screen_height = masterWindow.winfo_screenheight()
-res = "{a}x{b}".format(a=int(screen_width), b=int(screen_height))
 
-masterWindow.title("for senios-alpha")
-masterWindow.geometry(res)
-masterWindow.overrideredirect(True)
 
-#Master Frame
-masterBar = tkinter.Frame(masterWindow, height=int(screen_height/6))
-masterBar.pack_propagate(False) #zakazuje, aby se velikost baru fixne scaloval podle velikosti tlacitka
-masterBar.pack(side=tkinter.TOP, fill=tkinter.X)
+class DesktopFrame:
+    def __init__(self, masterFrame):
+        self.options1Button = None
+        self.backButton = None
+        self.menuButton = None
+        self.masterFrame = masterFrame
 
-#innerFrame
-menuBar = tkinter.Frame(masterBar, width=int(screen_width/6), bg='GREEN')
-menuBar.pack_propagate(False)
-menuBar.pack(side=tkinter.LEFT,fill=tkinter.Y)
-exitBar = tkinter.Frame(masterBar, width=int(screen_width/10), bg='BROWN')
-exitBar.pack_propagate(False)
-exitBar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-optionsBar = tkinter.Frame(masterBar, bg='YELLOW')
-optionsBar.pack_propagate(False)
-optionsBar.pack(expand=True, fill=tkinter.BOTH)
+        __fontSize = font.Font(family='Halvetica', size=36, weight=font.BOLD)
+        def menuClicked(): #schová "MENU" a ukáže "BACK"
+            print("menuClicked")
+            self.menuButton.pack_forget()
+            self.options1Button.pack(side=LEFT)
+            self.backButton.pack()
 
-fontPokus = tkFont.Font(family='Halvetica',size=36,weight=tkFont.BOLD)
-pokusButton = tkinter.Button(menuBar,text="MENU",width=int(screen_width/6), height=int(screen_height/6),font=fontPokus)
-pokusButton.pack()
+        def backClicked():#reverse oproti menuClicked()
+            print("backClicked")
+            self.menuButton.pack()
+            self.options1Button.pack_forget()
+            self.backButton.pack_forget()
 
-exitButtonPokus = tkinter.Button(exitBar, text="EXIT", width=int(screen_width / 6), height=int(screen_height / 6), font=fontPokus, command=masterWindow.destroy)
-exitButtonPokus.pack()
+        def createMenuButtons(fontSize):
+            print("we craftin menu")
+            self.menuButton = tkinter.Button(menuBar, text="MENU", width=int(masterFrame.winfo_screenwidth() / 6),height=int(masterFrame.winfo_screenwidth() / 6), command=menuClicked)
+            self.menuButton['font']= fontSize
+            self.menuButton.pack()
+            self.backButton = tkinter.Button(menuBar, text="BACK", width=int(masterFrame.winfo_screenwidth() / 6),height=int(masterFrame.winfo_screenheight() / 6), command=backClicked)
+            self.backButton['font'] = fontSize
 
-masterWindow.mainloop()
+        def createExitButton(fontSize):
+            print("we craftin exit")
+            exitButtonPokus = tkinter.Button(exitBar, text="EXIT", width=int(masterFrame.winfo_screenwidth() / 6),height=int(masterFrame.winfo_screenheight() / 6),command=self.masterFrame.destroy)
+            exitButtonPokus['font'] = fontSize
+            exitButtonPokus.pack()
+
+        def createOptionsButtons():
+            print("we craftin options")
+            self.options1Button = tkinter.Button(optionsBar, text="OPTIONS_1",height=int(masterFrame.winfo_screenheight() / 6))
+
+
+
+        # Main frame
+        masterBar = Frame(masterFrame, height=int(masterFrame.winfo_screenheight() / 6))
+        masterBar.pack_propagate(False)  # zakazuje, aby se velikost baru fixne scaloval podle velikosti tlacitka
+        masterBar.pack(side=TOP, fill=X)
+
+        # sekce pro menu
+        menuBar = Frame(masterBar, width=int(masterFrame.winfo_screenwidth() / 6))
+        menuBar.pack_propagate(False)
+        menuBar.pack(side=LEFT, fill=Y)
+
+        # sekce pro exit
+        exitBar = Frame(masterBar, width=int(masterFrame.winfo_screenwidth() / 6))
+        exitBar.pack_propagate(False)
+        exitBar.pack(side=RIGHT, fill=Y)
+
+        # sekce pro options
+        optionsBar = Frame(masterBar, bg='YELLOW')  # pak dat barvu pryč
+        optionsBar.pack_propagate(False)
+        optionsBar.pack(expand=True, fill=BOTH)
+
+        #volání defů pro tlačítka
+        createMenuButtons(__fontSize)
+        createOptionsButtons()
+        createExitButton(__fontSize)
+class App:
+    def __init__(self, masterWindow):
+        self.masterWindow = masterWindow
+        masterWindow.title("for senior")
+
+        screen_width = masterWindow.winfo_screenwidth()
+        screen_height = masterWindow.winfo_screenheight()
+        res = "{a}x{b}".format(a=int(screen_width), b=int(screen_height))
+
+        masterWindow.geometry(res)
+        masterWindow.overrideredirect(True)
+
+
 
 if __name__ == '__main__':
-    print("gui")
-    print(res)
+    root = Tk()
+    my_gui = App(root)
+    idk = DesktopFrame(root)
+    root.mainloop()
