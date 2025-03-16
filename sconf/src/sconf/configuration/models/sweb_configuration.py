2e47506ffb292aca76e21aa54b07f27073035fb0
Tarik Alkanan <78847793+TarikVUT@users.noreply.github.com>
2025-03-16 15:48:14 +0100

    Update sweb_configuration.py

--- sconf/src/sconf/configuration/models/sweb_configuration.py
+++ sconf/src/sconf/configuration/models/sweb_configuration.py
@@ -5,12 +5,12 @@ from typing import List
 @dataclass
 class SwebConfiguration:
     swebAllowedUrlListV2: List[dict] = field(default_factory=lambda : [
-        {"url1": "https://irozhlas.cz/", "icon1": "../sconf/icons/sweb_www_1.png"},
-        {"url2": "https://edition.cnn.com/", "icon2": "../sconf/icons/sweb_www_2.png"},
-        {"url3": "https://www.vut.cz/", "icon3": "../sconf/icons/sweb_www_3.png"},
-        {"url4": "https://google.com/", "icon4": "../sconf/icons/sweb_www_4.jpg"},
-        {"url5": "https://www.aktualne.cz/", "icon5": "../sconf/icons/sweb_www_5.png"},
-        {"url6": "https://www.denik.cz/", "icon6": "../sconf/icons/sweb_www_6.png"},
+        {"url1": "https://irozhlas.cz/", "icon1": "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_1.png"},
+        {"url2": "https://edition.cnn.com/", "icon2": "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_2.png"},
+        {"url3": "https://www.vut.cz/", "icon3": "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_3.png"},
+        {"url4": "https://google.com/", "icon4": "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_4.jpg"},
+        {"url5": "https://www.aktualne.cz/", "icon5": "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_5.png"},
+        {"url6": "https://www.denik.cz/", "icon6": "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_6.png"},
     ])
     urlsForWebsites: List[str] = field(default_factory=lambda: ["https://irozhlas.cz/",
                                                                 "https://edition.cnn.com/",
@@ -19,16 +19,16 @@ class SwebConfiguration:
                                                                 "https://www.aktualne.cz/",
                                                                 "https://www.denik.cz/"])
 
-    picturePaths: List[str] = field(default_factory=lambda: ["../sconf/icons/exit.png",
-                                                             "../sconf/icons/sweb_www_1.png",
-                                                             "../sconf/icons/sweb_www_2.png",
-                                                             "../sconf/icons/sweb_www_3.png",
-                                                             "../sconf/icons/sweb_www_4.png",
-                                                             "../sconf/icons/sweb_www_5.png",
-                                                             "../sconf/icons/sweb_www_6.png"])
-    allowedURL: str = "../sconf/phish/sweb_allowed_url.txt"
-    phishingDatabase: str = "../sconf/phish/sweb_phish.txt"
-    command_line_mail_script: str = "../smail/src/smail/connection/command_line_mail.py"
+    picturePaths: List[str] = field(default_factory=lambda: ["/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/exit.png",
+                                                             "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_1.png",
+                                                             "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_2.png",
+                                                             "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_3.png",
+                                                             "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_4.png",
+                                                             "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_5.png",
+                                                             "/run/archiso/airootfs/usr/lib/python3.13/site-packages/icons/sweb_www_6.png"])
+    allowedURL: str = "/run/archiso/airootfs/usr/lib/python3.13/site-packages/phish/sweb_allowed_url.txt"
+    phishingDatabase: str = "/run/archiso/airootfs/usr/lib/python3.13/site-packages/phish/sweb_phish.txt"
+    command_line_mail_script: str = "/run/archiso/airootfs/usr/lib/python3.13/site-packages/smail/connection/command_line_mail.py"
     phishingGithubDatabase: str = "https://github.com/mitchellkrogza/Phishing.Database/raw/master/ALL-phishing-domains.tar.gz/"
     sendPhishingWarning: bool = True
     phishingFormular: bool = True
